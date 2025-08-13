55b369dce8de1ea9bfe1c911b99724839d1c379e
MrHryhorii <hryhorii.kodehode@gmail.com>
2025-08-13 11:30:48 +0200

    task-1

--- script.js
+++ script.js
@@ -41,12 +41,19 @@ const separator = () => {
 /*
     1.Find all products in the "electronics" category that cost less than 200 – 
         these should receive an extra discount to be sold out quickly.
-    2.Make a list of the names of all the products - so Alva can print an advertising sheet for sales.
-    3.Find the names of all products in the "clothing" category – clothing always sells well,
-        and Alva will market these specifically.
-    4.Check if there are products that cost more than 1000 – the expensive products may be a little difficult to sell,
-        and Alva is considering providing special offers on them.
-    5.Rain out how much the entire warehouse is worth before sales start - So Alva knows how much she can earn.
+    2.Make a list of the names of all the products - 
+        so Alva can print an advertising sheet for sales.
+    3.Find the names of all products in the "clothing" category – 
+        clothing always sells well, and Alva will market these specifically.
+    4.Check if there are products that cost more than 1000 – 
+        the expensive products may be a little difficult to sell, and Alva is considering providing special offers on them.
+    5.Rain out how much the entire warehouse is worth before sales start - 
+        So Alva knows how much she can earn.
 */
 
 /* Task - 1 */
+log("Task - 1");
+const less200 = products.filter(item => item.price < 200 && item.category == "electronics");
+log("Electronics that cost less than 200");
+log(less200);
+separator();
